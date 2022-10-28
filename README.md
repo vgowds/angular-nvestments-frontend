873aa18755068f6cb7b68f0d57fc44e6b2b373f5
vgowds <vgowds@yahoo.com>
2022-10-28 14:53:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-nvestments-frontend
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-uccdul)
---
+++ src/app/products.ts
@@ -0,0 +1,34 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: string;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'FAARX',
+    price: '$5.9 B',
+    description: 'All-American Municipal Bond'
+  },
+  {
+    id: 2,
+    name: 'NHMRX',
+    price: '$19.4 B',
+    description: 'High Yield Municipal Bond'
+  },
+  {
+    id: 3,
+    name: 'NUVBX',
+    price: '$8.6 B',
+    description: 'Intermediate Duration Municipal Bond'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
